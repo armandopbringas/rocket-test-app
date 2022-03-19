24d80c2de58ce14704af4f81cebde687fc0f20fc
armanopbringas <bringas.armandop@gmail.com>
2022-03-19 11:50:26 -0600

    - import birth day form component and passing it addBirthDay function as prop - import birth day data component and passing it state as prop

--- src/app/app.js
+++ src/app/app.js
@@ -1,17 +1,23 @@
 import { useState } from 'react';
+import BirthDayData from '../components/bday-data';
+import BirthDayFrom from '../components/birthday';
 import NameForm from '../components/name';
 import NameData from '../components/name-data';
 import './app.css';
 
 const App = () => {
   const [ names, setNames ] = useState([]);
+  const [ bDays, setDay ] = useState([]);
   const addName = name => setNames( [...names, name] );
+  const addBirthDay = bDay => setDay( [...bDays, bDay] );
 
   return (
     <>
       <h3>Chat form app</h3>
       <NameForm addName={addName} />
       <NameData names={names} />
+      <BirthDayFrom addBirthDay={addBirthDay} />
+      <BirthDayData bDays={bDays} />
     </>
   );
 }
